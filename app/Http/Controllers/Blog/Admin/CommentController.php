bf1be447b103eb23e0382beb696e2944a93417c7
MarAbdullin <skiff27@yandex.ru>
2021-11-26 17:34:51 +0300

    b3

---
+++ app/Http/Controllers/Blog/Admin/CommentController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers\Blog\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use App\Http\Requests\CommentRequest;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    //права на доступ к действиям контроллера
+    public function __construct() {
+        $this->middleware('perm:manage-comments')->only(['index', 'show']);
+        $this->middleware('perm:edit-comment')->only('update');
+        $this->middleware('perm:publish-comment')->only(['enable', 'disable']);
+        $this->middleware('perm:delete-comment')->only('destroy');
+    }
+
+    //Показывает список всех комментариев
+    public function index()
+    {
+        // $comment = Comment::where('id', 1)->first();
+        // $post = $comment->post;
+        // $comments = $post->comments;
+        // dd($comments);
+        $comments = Comment::orderBy('created_at', 'desc')->paginate(10);
+        return view('admin.comment.index', compact('comments'));
+        
+    }
+
+    
+    public function create()
+    {
+        //
+    }
+
+    
+    public function store(Request $request)
+    {
+        //
+    }
+
+    //Просмотр комментария к посту блога
+    public function show(Comment $comment)
+    {
+        // сигнализирует о том, что это режим пред.просмотра
+        session()->flash('preview', 'yes');
+        // это тот пост блога, к которому оставлен комментарий
+        $post = $comment->post;
+        // коллекция всех комментариев к этому посту блога
+        $comments = $post->comments()->orderBy('created_at')->paginate();
+        
+        return view('admin.post.show', compact('post', 'comments'));
+    }
+
+    
+    public function edit(Comment $comment)
+    {   // нужно сохранить flash-переменную, которая сигнализирует о том,
+        // что кнопка редактирования была нажата в режиме пред.просмотра
+        session()->keep('preview');
+
+        return view('admin.comment.edit', compact('comment'));
+    }
+
+    //Обновляет комментарий в базе данных
+    public function update(CommentRequest $request, Comment $comment)
+    {
+        $comment->update($request->all());
+        return $this->redirectAfterUpdate($comment);
+    }
+
+    public function enable(Comment $comment)
+    {
+        $comment->enable();
+
+        $redirect = back();
+        if(session('preview')){
+            $rediret = $redirect->withFragment('comment-list');
+        }
+
+        return $redirect->with(['success' => 'Комментарий был опубликован']);
+
+    }
+
+    public function disable(Comment $comment)
+    {
+        $comment->disable();
+
+        $redirect = back();
+        if(session('preview')){
+            $rediret = $redirect->withFragment('comment-list');
+        }
+
+        return $redirect->with(['success' => 'Комментарий был снят с публикации']);
+    }
+
+    public function destroy(Comment $comment)
+    {
+        $comment->delete();
+
+        $redirect = back();
+        if(session('preview')){
+            $rediret = $redirect->withFragment('comment-list');
+        }
+
+        return $redirect->with(['success' => 'Комментарий был удален']);
+    }
+
+    //Выполянет редирект после обновления
+    public function redirectAfterUpdate(Comment $comment)
+    {
+        $redirect = redirect();
+        // кнопка редактирования может быть нажата в режиме пред.просмотра
+        // или в панели управления блогом, поэтому и редирект будет разный
+        if(session('preview')){
+            $redirect = $redirect->route(
+                'admin.comment.show',
+                ['comment' => $comment->id, 'page' => $comment->adminPageNumber()])
+                ->withFragment('comment-list');
+        }
+        else{
+            $redirect = $redirect->route('admin.comment.index');
+        }
+
+        return $redirect->with(['success' => 'Комментарий был успешно исправлен']);
+    }   
+}
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Post;
 use App\Models\User;
 
@@ -11,6 +12,15 @@ use App\Models\User;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'post_id',
+        'published_by',
+        'content',
+    ];
+
+    protected $perPage = 5;
 
     /*
       Связь модели Comment с моделью Post, позволяет получить
@@ -24,11 +34,66 @@ class Comment extends Model
     /*
       Связь модели Comment с моделью User, позволяет получить
       пользователя комментария
-     */
+    */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+
+    /*
+      Связь модели Comment с моделью User, позволяет получить
+      пользователя опубликовавшего комментарий
+    */
+    public function editor()
+    {
+        return $this->belongsTo(User::class, 'published_by');
+    }
+
+    // Разрешить публикацию комментария блог
+    public function enable()
+    {
+        $this->published_by = Auth::user()->id;
+        $this->update();
+    }
+
+    // Запретить публикацию комментария блог
+    public function disable()
+    {
+        $this->published_by = null;
+        $this->update();
+    }
+
+     /*
+      Номер страницы пагинации, на которой расположен комментарий;
+      учитываются все комментарии, в том числе не опубликованные
+     */
+    public function adminPageNumber()
+    {
+         $post = $this->post;
+         $comments = $post->comments()->orderBy('created_at')->get();
+
+        if($comments->count() == 0){
+            return 1;  //если в коллекции нет комментариев 
+        } 
+
+        if($comments->count() <= $this->getPerPage()){
+            return 1; // если в коллекции комментариев меньше чем количества элементов на странице
+        } 
+
+        foreach($comments as $index => $comment){
+            if($this->id == $comment->id){
+                break;
+            }
+        }
+        return (int) ceil(($index+1) / $this->getPerPage()); // вернет номер страницы(индекс комментария в коллекции / количество элементов на странице)
+    }
+
+    //Возвращает true, если публикация разрешена
+    public function isVisible()
+    {
+        return ! is_null($this->published_by);
+    }
+
 
     
 
