a02d7d2779583cdf53cc2d1108f61fc392efb722
MarAbdullin <skiff27@yandex.ru>
2021-12-03 14:52:05 +0300

    v1.16

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,17 +1,19 @@
-@extends('layout.site', ['title' => 'Личный кабинет'])
+@extends('layout.user', ['title' => 'Личный кабинет'])
 
 @section('content')
     <h1>Личный кабинет</h1>
-    <p>Добрый день {{ auth()->user()->name }}!</p>
-    <p>Это личный кабинет пользователя сайта.</p>
-@role('root')
-<ul class="nav nav-pills">
-    <li class="nav-item">
-        <a class="nav-link active" href="{{ route('admin.index') }}">Панель администратора</a> <!-- активная ссылка -->
-    </li>
-</ul>
-@else
-<p>не имеет</p>
-@endrole
 
+    <p>Добрый день {{ auth()->user()->name }}!</p>
+
+    @perm('create-post')
+        <a href="{{ route('user.post.create') }}" class="btn btn-success">
+            Новая публикация
+        </a>
+    @endperm
+        <a href="{{ route('user.post.index') }}" class="btn btn-primary">
+            Ваши публикации
+        </a>
+        <a href="{{ route('user.comment.index') }}" class="btn btn-primary">
+            Ваши комментарии
+        </a>
 @endsection
